483484b970b2a855b525c30557eed717c41f057c
Paul Madden <paul.madden@noaa.gov>
2025-07-15 00:33:15 +0000

    Exercise 1.11

---
+++ 1.11.scm
@@ -0,0 +1,24 @@
+(define (fr n)
+  (cond ((< n 3) n)
+        (else (+ (fr (- n 1))
+                 (fr (- n 2))
+                 (fr (- n 3))))))
+
+(fr 1)
+(fr 2)
+(fr 3)
+(fr 4)
+(fr 10)
+
+(define (fi n)
+  (define (fi-iter a b c n)
+    (if (= 0 n)
+        c
+        (fi-iter (+ a b) (+ b c) b (- n 1))))
+  (fi-iter 2 1 0 n))
+
+(fr 1)
+(fr 2)
+(fr 3)
+(fr 4)
+(fr 10)
