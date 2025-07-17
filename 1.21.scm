8c85aaa9d87c4ead97a1ccc7d6f4ca301091b9a3
Paul Madden <paul.madden@noaa.gov>
2025-07-17 21:52:31 +0000

    Exercise 1.21

---
+++ 1.21.scm
@@ -0,0 +1,10 @@
+(define (smallest-divisor n)
+  (define (find-divisor n test-divisor)
+    (define (divides? a b) (= (remainder b a) 0))
+    (define (square n) (* n n))
+    (cond ((> (square test-divisor) n) n)
+          ((divides? test-divisor n) test-divisor)
+          (else (find-divisor n (+ test-divisor 1)))))
+  (find-divisor n 2))
+
+(map smallest-divisor '(199 1999 19999))
