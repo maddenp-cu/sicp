397dcaf6790ec6ed7b30d3606e11b9f2de0ef0b9
Paul Madden <paul.madden@noaa.gov>
2025-07-31 06:07:50 +0000

    Exercise 1.31

---
+++ 1.31.scm
@@ -0,0 +1,32 @@
+(define (product term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (* result (term a)))))
+  (iter a 1))
+
+(define (identity n) n)
+(define (inc n) (+ n 1))
+
+(define (factorial n)
+  (product identity 1 inc n))
+
+(factorial 5)
+
+(define (pi f n)
+  (define (plus-2 n) (+ n 2))
+  (define (term n) (* (/ (- n 1) n) (/ (+ n 1) n)))
+  (* 4.0 (f term 3 plus-2 n)))
+
+(pi product 100)
+(pi product 1000)
+(pi product 10000)
+
+(define (product-r term a next b)
+  (define (iter a)
+    (if (> a b)
+        1
+        (* (term a) (iter (next a)))))
+  (iter a))
+
+(pi product-r 10000)
